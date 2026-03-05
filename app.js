ef9fd2c55479b5e4589a806fb920bd88c68f99b7
Hüseyin ABA <abahuseyin992@gmail.com>
2026-03-05 20:19:14 +0300

    Setup EJS template engine and views folder

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
-const path = require('path');
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log("Middleware Log 1");
-  next();
-};
+//TEMPLATE ENGINE
 
-const myLogger2 = (req, res, next) => {
-  console.log("Middleware Log 2");
-  next();
-};
+app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static("public"));
-app.use(myLogger);
-app.use(myLogger2);
 
+//ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+
+  res.render("add");
 });
 
 const port = 3000;
