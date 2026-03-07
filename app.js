e72221a1c97998fe7e34e276e4679218e86fd023
Hüseyin ABA <abahuseyin992@gmail.com>
2026-03-07 21:13:01 +0300

    Restructure application using MVC architecture

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const ejs = require('ejs');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
-const path = require('path');
-const fs = require('fs');
-const Photo = require('./models/Photo');
+
+// YENİ: Controller'larımızı projemize dahil ediyoruz
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -24,81 +25,16 @@ app.use(methodOverride('_method', {
   methods: ['POST', 'GET']
 }));
 
-//ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos
-  });
-});
+// ROUTES - İşleri Controller'lara Devrettik
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-
-app.get("/photos/:id", async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo", {
-    photo
-  });
-});
-
-app.post("/photos", async (req, res) => {
-  const uploadDir = 'public/uploads';
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadedImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
-
-  uploadedImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadedImage.name,
-    });
-    res.redirect('/');
-  });
-});
-
-app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit', {
-    photo
-  });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-
-  res.redirect(`/photos/${req.params.id}`);
-});
-
-// YENİ EKLENEN KISIM: Fotoğraf Silme Rotası (DELETE)
-app.delete('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  
-  // Fotoğrafın dosya yolunu buluyoruz
-  let deletedImage = __dirname + '/public' + photo.image;
-  
-  // Eğer sunucuda (uploads klasöründe) böyle bir dosya gerçekten varsa, fiziksel olarak siliyoruz
-  if (fs.existsSync(deletedImage)) {
-    fs.unlinkSync(deletedImage);
-  }
-
-  // Veritabanından fotoğrafı siliyoruz
-  await Photo.findByIdAndDelete(req.params.id);
-  
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
